c4faf2c39e55aaf69e7081e7f707d83e575a2fa5
Ansis Martinsons <ansis.martinsons@gmail.com>
2017-09-24 05:44:07 +0000

    Changes in baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+// $config['base_url'] = 'https://ci-tutorial-aberkalns.c9users.io/';
+$config['base_url'] = 'https://morning-reaches-17904.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
